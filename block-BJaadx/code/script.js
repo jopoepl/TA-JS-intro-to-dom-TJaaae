dfb5ab45f03a82a3dc4d0dfe0b9628c38f396782
jopoepl <joel_joseph93@outlook.com>
2022-06-01 12:10:10 +0530

    Assignment: Selecting Elements

--- block-BJaadx/code/script.js
+++ block-BJaadx/code/script.js
@@ -1,15 +1,35 @@
 // 1. Change the title of the page to `Hello AltCampus!`
 
+document.head.children[2].innerText = "Hello AltCampus"
+
+
 // 2. Select the element using the children property:
 
 //    - Select the `h1` element and change the value to `Learning DOM`
 
+document.body.children[0] = h1;
+h1.innerText = `Learning DOM`;
+
+
+
 //    - Select the first `li` element inside the `ul` with class `topics` and change the innerText to `all about document`
+
+topics[0].children[0].innerText = `all about document`
+
+
+
 //    - Select the input element with name `email`
+
+document.getElementById(`email`);
 
 // 3. Log the number (using console.log) of children of all the `li` element inside the ul with class `topics`
 
+console.log(topics[0].children)
+
+
 // 4. Select the first input using the `type` selector and store them in variable named `emailInput`
+
+
 
 // 5. Select the ul element using class selector and store in `topics`
 
